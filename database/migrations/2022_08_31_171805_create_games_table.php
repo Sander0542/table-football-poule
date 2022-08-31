40c8ce2207a28328dc59a45f55928332f741d45a
Sander Jochems <sanderjochems@hotmail.nl>
2022-08-31 20:00:07 +0200

    Add team_id to games table

--- database/migrations/2022_08_31_171805_create_games_table.php
+++ database/migrations/2022_08_31_171805_create_games_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('team_id')->constrained();
             $table->timestamp('played_at');
             $table->integer('score_blue');
             $table->integer('score_red');
