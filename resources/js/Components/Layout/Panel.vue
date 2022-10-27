147c69162d65850b0f4c0742355b50858417434f
Sander Jochems <sanderjochems@hotmail.nl>
2022-10-27 17:59:14 +0200

    Fix Panel

--- resources/js/Components/Layout/Panel.vue
+++ resources/js/Components/Layout/Panel.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
-        <div class="px-4 py-5 sm:px-6">
+        <div v-if="$slots.header" class="px-4 py-5 sm:px-6">
             <slot name="header"/>
         </div>
         <div class="px-4 py-5 sm:p-6">
